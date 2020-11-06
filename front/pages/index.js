3115a0d03d883e5375302e8e7c88d3073fffde8f
eunbi <silver23rain@gmail.com>
2020-11-06 15:44:08 +0900

    :wrench: antd 적용

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
+import AppLayout from "../componets/AppLayout";
 
 const Home = () => {
     return (
         <>
-            <Link href="/about">
-                <a>about</a>
-            </Link>
-            <div>Hello Next</div>
+            <Head>
+                <title>NodeBird</title>
+                <link
+                    rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.4.3/antd.css"
+                />
+            </Head>
+            <AppLayout>
+                <Link href="/about">
+                    <a>about</a>
+                </Link>
+                <div>Hello Next</div>
+            </AppLayout>
         </>
     );
 };
