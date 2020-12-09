efca33d92f936abc0b2ed3b7ee39ed1070522141
eunbi <silver23rain@gmail.com>
2020-12-09 17:47:11 +0900

    :sparkles: _app.js proptype 추가

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,22 +1,27 @@
-import React from "react";
-import Head from "next/head";
-import AppLayout from "../componets/AppLayout";
+import React from 'react';
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import AppLayout from '../componets/AppLayout';
 
 const NodeBird = ({ Component }) => {
-    return (
-        <>
-            <Head>
-                <title>NodeBird</title>
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.4.3/antd.css"
-                />
-            </Head>
-            <AppLayout>
-                <Component />
-            </AppLayout>
-        </>
-    );
+	return (
+		<>
+			<Head>
+				<title>NodeBird</title>
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.4.3/antd.css"
+				/>
+			</Head>
+			<AppLayout>
+				<Component />
+			</AppLayout>
+		</>
+	);
+};
+
+NodeBird.prototypes = {
+	Component: PropTypes.element,
 };
 
 export default NodeBird;
