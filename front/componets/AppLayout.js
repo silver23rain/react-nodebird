d1e64f39aa941a74304db0fa0eb3b8a0139dd75b
eunbi <silver23rain@gmail.com>
2020-12-17 01:50:49 +0900

    :sparkles: 로그인폼 생성

--- front/componets/AppLayout.js
+++ front/componets/AppLayout.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
-import { Menu, Input, Button, Row, Col, Card } from 'antd';
-import Avatar from 'antd/lib/avatar/avatar';
+import { Menu, Input, Button, Row, Col, Card, Form, Avatar } from 'antd';
+import LoginForm from './LoginForm';
 
 const dummy = {
 	nickname: '박은비',
 	Post: [],
 	Followings: [],
 	Followers: [],
+	isLoggedIn: false,
 };
 const AppLayout = ({ children }) => {
 	return (
@@ -25,37 +26,31 @@ const AppLayout = ({ children }) => {
 					</Link>
 				</Menu.Item>
 				<Menu.Item key="mail">
-					<Input.Search
-						enterButton
-						style={{ verticalAlign: 'middle' }}
-					/>
+					<Input.Search enterButton style={{ verticalAlign: 'middle' }} />
 				</Menu.Item>
 			</Menu>
-			<Link href="/signup">
-				<a>
-					<Button>회원가입</Button>
-				</a>
-			</Link>
 			<Row>
 				<Col xs={24} md={6}>
-					<Card
-						actions={[
-							<div key="twit">
-								짹짹 <br /> {dummy.Post.length}
-							</div>,
-							<div key="following">
-								팔로잉 <br /> {dummy.Followings.length}
-							</div>,
-							<div key="follower">
-								팔로워 <br /> {dummy.Followers.length}
-							</div>,
-						]}
-					>
-						<Card.Meta
-							avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
-							title={dummy.nickname}
-						/>
-					</Card>
+					{dummy.isLoggedIn ? (
+						<Card
+							actions={[
+								<div key="twit">
+									{' '}
+									짹짹 <br /> {dummy.Post.length}{' '}
+								</div>,
+								<div key="following">
+									팔로잉 <br /> {dummy.Followings.length}
+								</div>,
+								<div key="follower">
+									팔로워 <br /> {dummy.Followers.length}
+								</div>,
+							]}
+						>
+							<Card.Meta avatar={<Avatar>{dummy.nickname[0]}</Avatar>} title={dummy.nickname} />
+						</Card>
+					) : (
+						<LoginForm />
+					)}
 				</Col>
 				<Col xs={24} md={12}>
 					{children}
@@ -64,7 +59,6 @@ const AppLayout = ({ children }) => {
 					세번째
 				</Col>
 			</Row>
-			{children}
 		</div>
 	);
 };
---
+++ front/componets/LoginForm.js
@@ -0,0 +1,42 @@
+import { Button, Form, Input } from 'antd';
+import Link from 'next/link';
+import React, { useCallback } from 'react';
+import { useInput } from '../pages/signup';
+
+const LoginForm = () => {
+	const [id, onChangeId] = useInput('');
+	const [passeord, onChangePassword] = useInput('');
+	const onSubmitForm = useCallback(
+		(e) => {
+			e.preventDefault;
+			console.log({ id, passeord });
+		},
+		[id, passeord]
+	);
+	return (
+		<Form onFinish={onSubmitForm}>
+			<div>
+				<label htmlFor="user-id">아이디</label>
+				<br />
+				<Input name="user-id" value={id} onChange={onChangeId} required />
+			</div>
+			<div>
+				<label htmlFor="user-password">비밀번호</label>
+				<br />
+				<Input name="user-password" type="password" value={passeord} onChange={onChangePassword} required />
+			</div>
+			<div>
+				<Button type="primary" htmlType="submit" loading={false}>
+					로그인
+				</Button>
+				<Link href="/signup">
+					<a>
+						<Button>회원가입</Button>
+					</a>
+				</Link>
+			</div>
+		</Form>
+	);
+};
+
+export default LoginForm;
