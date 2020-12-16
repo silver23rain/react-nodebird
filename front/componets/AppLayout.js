363cf83e60fb4d678412282a75a7771b8d487816
eunbi <silver23rain@gmail.com>
2020-12-17 01:02:50 +0900

    :sparkles: AppLatout에 antd 그리드 적용

--- front/componets/AppLayout.js
+++ front/componets/AppLayout.js
@@ -1,40 +1,75 @@
-import React from "react";
-import Link from "next/link";
-import PropTypes from "prop-types";
-import { Menu, Input, Button } from "antd";
+import React from 'react';
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import { Menu, Input, Button, Row, Col, Card } from 'antd';
+import Avatar from 'antd/lib/avatar/avatar';
 
+const dummy = {
+	nickname: '박은비',
+	Post: [],
+	Followings: [],
+	Followers: [],
+};
 const AppLayout = ({ children }) => {
-    return (
-        <div>
-            <Menu mode="horizontal">
-                <Menu.Item key="home">
-                    <Link href="/">
-                        <a>노드버드</a>
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key="profile">
-                    <Link href="/profile">
-                        <a>프로필</a>
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key="mail">
-                    <Input.Search
-                        enterButton
-                        style={{ verticalAlign: "middle" }}
-                    />
-                </Menu.Item>
-            </Menu>
-            <Link href="/signup">
-                <a>
-                    <Button>회원가입</Button>
-                </a>
-            </Link>
-            {children}
-        </div>
-    );
+	return (
+		<div>
+			<Menu mode="horizontal">
+				<Menu.Item key="home">
+					<Link href="/">
+						<a>노드버드</a>
+					</Link>
+				</Menu.Item>
+				<Menu.Item key="profile">
+					<Link href="/profile">
+						<a>프로필</a>
+					</Link>
+				</Menu.Item>
+				<Menu.Item key="mail">
+					<Input.Search
+						enterButton
+						style={{ verticalAlign: 'middle' }}
+					/>
+				</Menu.Item>
+			</Menu>
+			<Link href="/signup">
+				<a>
+					<Button>회원가입</Button>
+				</a>
+			</Link>
+			<Row>
+				<Col xs={24} md={6}>
+					<Card
+						actions={[
+							<div key="twit">
+								짹짹 <br /> {dummy.Post.length}
+							</div>,
+							<div key="following">
+								팔로잉 <br /> {dummy.Followings.length}
+							</div>,
+							<div key="follower">
+								팔로워 <br /> {dummy.Followers.length}
+							</div>,
+						]}
+					>
+						<Card.Meta
+							avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+							title={dummy.nickname}
+						/>
+					</Card>
+				</Col>
+				<Col xs={24} md={12}>
+					{children}
+				</Col>
+				<Col cs={24} ms={6}>
+					세번째
+				</Col>
+			</Row>
+			{children}
+		</div>
+	);
 };
 
 AppLayout.propTypes = {
-    children: PropTypes.node,
+	children: PropTypes.node,
 };
 export default AppLayout;
